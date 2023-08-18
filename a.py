8925703dfe95d9186c986a4f47d7ae6349f732b1
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-17 21:53:55 -0700

    Update a.py

--- a.py
+++ a.py
@@ -3,16 +3,10 @@ lst=[]
 for i in range(int(input())):
     w = input()
     size=len(w)
-    '''
     if(size > 10):
         lst.append(w[0]+str(size-2)+w[size-1])
     else:
         lst.append(w)
-    '''
-    #result = (w[0]+str(size-2)+w[size-1]) if size > 10 else w
-    lst.append((w[0]+str(size-2)+w[size-1]) if size > 10 else w)
-
-
     
 for i in lst:
     print(i)
