f9860f8e522879201ee053722f7bdb1e629a60b6
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-21 14:50:00 -0700

    Create StonesOnTheTable266A.py

---
+++ StonesOnTheTable266A.py
@@ -0,0 +1,36 @@
+l=int(input())
+lst=list(input())
+def rec(l,lst):
+    x=0
+    lon=len(lst)
+    for i in range(0,lon//2):
+        if(lst[i] == lst[i+1]):
+            print(lst.pop(i+1))
+            x += 1
+    
+    if(len(lst) == len or x == 0):
+        return lst
+        
+    else:
+        return rec(len(lst),lst)
+        
+        
+    
+    
+print(rec(l,lst))
+
+def rec1(l,lst):
+    x=0
+    lon=len(lst)
+    for i in range(lon-1,lon//2,-1):
+        if(lst[i] == lst[i-1]):
+            print(lst.pop(i-1))
+            x += 1
+    
+    if(len(lst) == len or x == 0):
+        return lst
+        
+    else:
+        return rec(len(lst),lst)
+   
+print(rec1(len(rec(l,lst)),rec(l,lst))) 
