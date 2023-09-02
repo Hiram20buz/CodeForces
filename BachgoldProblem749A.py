5090b6b0e1d30e16925d09e6cd273efd641c2e09
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-09-01 23:24:20 -0700

    Update BachgoldProblem749A.py

--- BachgoldProblem749A.py
+++ BachgoldProblem749A.py
@@ -1,15 +1,16 @@
 n=int(input())
 lst=[]
-def x(lst,n):
-    if(n % 2 !=0):
-        lst.append(3)
-        
-    while(sum(lst)<n):
-        lst.append(2)
-        if sum(lst) > n:
-            lst.pop()
-            break
-        
-x(lst,n) 
+x=n//2
+'''
+if(n % 2 !=0):
+    lst.append(3)
+    lst.extend([2] * (x-1))
+    
+else:
+    lst.extend([2] * (x))
+   
+''' 
+lst.extend([3] + [2] * (x - 1)) if n % 2 != 0 else lst.extend([2] * x)
+
 print(len(lst))
 print(*lst)
