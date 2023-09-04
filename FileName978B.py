170bb9e2b012833fe5e9655ab1722b129ade3b37
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-09-04 12:09:02 -0700

    Update FileName978B.py

--- FileName978B.py
+++ FileName978B.py
@@ -11,20 +11,6 @@ def kill(lst):
         remove_char(lst, 'x')
     
 if re.search('xxx', "".join(total)):
-    #print('xxx in string')
     kill(total)
-else:
-    #print('Not xxx in string')
-    print(0)
     
-print(total)  
-'''
-import re
-lst = ['x', 'x', 'x', 'i', 'i', 'i']
-def remove_char(lst, char):
-    del lst[lst.index(char)]
-     
-while (re.search('xxx', "".join(lst))):
-    remove_char(lst, 'x')
-    print(lst)
-'''
+print(n-len(total)) 
