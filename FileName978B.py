8aff0b7edae5b79fa644f3bf84c6e6442b28cdad
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-09-04 12:03:06 -0700

    Update FileName978B.py

--- FileName978B.py
+++ FileName978B.py
@@ -6,13 +6,18 @@ lst=[]
 def remove_char(lst, char):
     del lst[lst.index(char)]
     
+def kill(lst):     
+    while (re.search('xxx', "".join(lst))):
+        remove_char(lst, 'x')
+    
 if re.search('xxx', "".join(total)):
-    print('xxx in string')
+    #print('xxx in string')
+    kill(total)
 else:
-    print('Not xxx in string')
+    #print('Not xxx in string')
     print(0)
     
-    
+print(total)  
 '''
 import re
 lst = ['x', 'x', 'x', 'i', 'i', 'i']
