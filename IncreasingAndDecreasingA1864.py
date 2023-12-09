0eb46f747175d773a7373a7e6d69ca0f57dd4b58
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-12-08 22:36:02 -0800

    Update IncreasingAndDecreasingA1864.py

--- IncreasingAndDecreasingA1864.py
+++ IncreasingAndDecreasingA1864.py
@@ -2,3 +2,17 @@
 A. Increasing and Decreasing
 Ascending with decreasing difference: [1, 6, 10, 13, 15]
 '''
+t = int(input().strip())
+for _ in range(t):
+    x, y, n = map(int, input().strip().split())
+    cnt = 1
+    v = [0] * n
+    v[-1] = y
+    v[0] = x
+    for i in range(n - 2, 0, -1):
+        v[i] = v[i + 1] - cnt
+        cnt += 1
+    if v[1] - v[0] > v[2] - v[1]:
+        print(' '.join(map(str, v)))
+    else:
+        print(-1)
