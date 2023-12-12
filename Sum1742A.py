b3d0a68d5ae7c992830bd9f8d7824d9db385a38e
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-12-11 22:53:52 -0800

    Update Sum1742A.py

--- Sum1742A.py
+++ Sum1742A.py
@@ -1,15 +1,17 @@
-input_list = input().split()
-
-result_list = list(map(int, input_list))
-
 def check_sum_of_other_two(nums):
     a, b, c = nums
     return a == b + c or b == a + c or c == a + b
-
-
-result = check_sum_of_other_two(result_list)
-
-if result:
-    print("YES")
-else:
-    print("NO")
+    
+ans = []
+for i in range(int(input())):
+    input_list = input().split()
+    result_list = list(map(int, input_list))
+    result = check_sum_of_other_two(result_list)
+    
+    if result:
+        ans.append("YES")
+    else:
+        ans.append("NO")
+        
+for i in ans:
+    print(i)
