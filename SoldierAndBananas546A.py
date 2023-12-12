88cb4871d789bf26c5871fb83fd61951fbff0894
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-12-11 22:18:48 -0800

    Update SoldierAndBananas546A.py

--- SoldierAndBananas546A.py
+++ SoldierAndBananas546A.py
@@ -3,7 +3,13 @@ input_list = input().split()
 result_list = list(map(int, input_list))
 
 suma = 0
-for i in range(1,result_list[2]+1):
+i = 1
+while(i <= result_list[2]):
     suma = suma + result_list[0] * i
-    
-print(abs(suma - result_list[1]))
+    i = i+1
+    #print(suma)
+  
+if (suma < result_list[1]):
+    print(0)
+else:
+    print(abs(suma - result_list[1]))
